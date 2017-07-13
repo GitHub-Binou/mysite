15aab77b57f6ce504dcf8e0c2aa0f9617c4b5085
emerging <40113846@qq.com>
2017-07-13 13:45:50 +0800

    1

--- frontend/src/components/thread/moderation/thread/is-visible.js
+++
@@ -1,9 +0,0 @@
-export default function(thread) {
-  return !!(
-    (thread.acl.can_approve && thread.is_unapproved) ||
-    thread.acl.can_close ||
-    thread.acl.can_hide ||
-    thread.acl.can_move ||
-    thread.acl.can_pin
-  );
-}
--- frontend/src/components/threads-list/thread/details/index.js
+++
@@ -1,6 +0,0 @@
-/* jshint ignore:start */
-import BottomDetails from './bottom';
-import TopDetails from './top';
-
-export { BottomDetails };
-export { TopDetails };
